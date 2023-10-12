c167c69bdea230b57307b11e2f21b1e706adcc62
MiravBhojani <95929083+MiravBhojani@users.noreply.github.com>
2023-10-12 11:50:13 +0300

    Change in characters

--- Lab2c-SentimentAnalysis.R
+++ Lab2c-SentimentAnalysis.R
@@ -430,7 +430,7 @@ head(evaluation_likes_and_wishes, 10)
 # Function to remove special characters and convert all text to a standard
 # lower case
 remove_special_characters <- function(doc) {
-  gsub("[^a-zA-Z0-9 ]", "", doc, ignore.case = TRUE)
+  gsub("[^a-zA-Z ]", "", doc, ignore.case = TRUE)
 }
 
 evaluation_likes_and_wishes$Likes <- sapply(evaluation_likes_and_wishes$Likes,
